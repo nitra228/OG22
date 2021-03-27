9322c22ed778e62e615dc871030f723e26d99f15
nitra228 <77109602+nitra228@users.noreply.github.com>
2021-03-27 09:33:31 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const TOKEN = "eddedbe4d5f5810812cdbba68b4e0a9daae1d9031e7b092e8ef7ce5fa28dfeb93444b5879ac076a6cb4ba"; // 孝芯泻械薪 谐褉褍锌锌褘, 胁 泻芯褌芯褉芯泄 斜褍写械褌 褋褌芯褟褌褜 斜芯褌
+const GROUP_ID = 203568751 // 袗泄写懈 谐褉褍锌锌褘, 胁 泻芯褌芯褉芯泄 斜褍写械褌 褋褌芯褟褌褜 斜芯褌
+const BTN_TEXT = ["vto.pe", "vkbot.ru", "vkbot.ru"];
+const TIME = 500;
+const CHAT_SPAM = ["НА ВАС НАПАЛО И.Ш.К СДАЙТЕСЬ ИЛИ БУДИТЕ УНИЧТОЖЕНЫ",
+"ВЫ БУДИТЕ УНИЧТОЖЕНЫ"
+"💣💣💣💣💣💣💣💣💣💣💣💣💣💣💣💣💣"
+"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzhdznndnxjzjsndnzkzksndmxnzndnndmdnxnxxnzmmddjdnxnxnxnnxndndnxnxndndndnxndndndnxndndndnxndndndnndnxndndndndndndnxnxndnndnxndndnxnxnndndnxnzncnxndnfnxnxndnnxnxnxnxnxndndnxbdnddjdndndndndnndndbfhfhbbdbdbdhdhdbdnnddnjdskkwkeodoxodkdkdkkdkdkskskskskskskskmsmsmsmdmdmxmmxmxmxmxmxmdmdmdmdmdnnfcuuskskalaksjxjxjdjdndnndmdmdmzmzmxmxmmdmdndndndndndndndndnndnndndnddjskammsmdkxksmsmsmsmmsmsmeodicucjrnenmsmxkfnrnsmxmnfnrmsmmxndndndndndnndndndndndndnndndnxjxhchfnemsmxkkcndnrndndnndnensnsmzkxjjfnensnsmzmxjjchdnennsnznxndnamzkcjnenen"
+const HELLO_TEXT = `SHURMOVIK. 
+`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
